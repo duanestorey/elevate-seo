ae8ff806f4695d97e93bf785297d377e1b2e3587
Unknown <duanestorey@gmail.com>
2018-10-15 18:00:00 +0200

    Updates

--- src/page-cache.php
+++ src/page-cache.php
@@ -105,8 +105,8 @@ class ElevatePageCache {
 				header( 'ETag: ' . $this->_get_cache_key() );
 
 				$last_modified = filemtime( $this->_get_cache_file_path() );
-				header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s T', $last_modified ) );
-				
+				//header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s T', $last_modified ) );
+
 				http_response_code( 200 ); 
 
 				echo $decoded_cache_info->body;
