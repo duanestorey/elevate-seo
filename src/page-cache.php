b2398411f3ebc6a090bbdbba73b2f12e809e7ad7
Unknown <duanestorey@gmail.com>
2018-10-15 17:49:36 +0200

    Updates

--- src/page-cache.php
+++ src/page-cache.php
@@ -101,7 +101,7 @@ class ElevatePageCache {
 				header( 'X-Elevate-Cache: ' . ELEVATE_PAGE_CACHE_VERSION . '/Hit' );
 
 				header_remove( 'Cache-Control' );
-				header( 'Cache-Control: private, must-revalidate, no-cache' );
+				header( 'Cache-Control: private, must-revalidate, no-cache, max-age=0' );
 				header( 'ETag: ' . $this->_get_cache_key() );
 
 				http_response_code( 200 ); 
