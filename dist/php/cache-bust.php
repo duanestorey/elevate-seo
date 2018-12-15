959f55ee9360647bcf6d255072c88a4d47a6b1b9
Unknown <duanestorey@gmail.com>
2018-12-15 23:55:17 +0000

    Updates

--- dist/php/cache-bust.php
+++ dist/php/cache-bust.php
@@ -1,4 +1,4 @@
 <?php
 
-define( 'ELEVATE_CACHE_VERSION', '39b27754abcc' );
-define( 'ELEVATE_BUILD_VERSION', '5a7a6' );
+define( 'ELEVATE_CACHE_VERSION', '51f3f752b85f' );
+define( 'ELEVATE_BUILD_VERSION', 'd64f7' );
