0850d47a261e224ee8a395870e243d0384af7a40
Unknown <duanestorey@gmail.com>
2019-02-20 18:47:18 +0400

    Updates

--- dist/php/cache-bust.php
+++ dist/php/cache-bust.php
@@ -1,4 +1,4 @@
 <?php
 
-define( 'ELEVATE_CACHE_VERSION', '51f3f752b85f' );
-define( 'ELEVATE_BUILD_VERSION', 'd64f7' );
+define( 'ELEVATE_CACHE_VERSION', '5fd5e3267ebd' );
+define( 'ELEVATE_BUILD_VERSION', 'd3482' );
